59a683be1de65f694f654ecac4f4d862fe1b41ed
Michael Hohn <hohn@github.com>
2023-05-24 16:38:09 -0700

    wip: directory restructuring

--- build-database.sh
+++ build-database.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SRCDIR=$(pwd)/session-db
+SRCDIR=$(pwd)/src/session-db
 DB=$SRCDIR/cpp-runtime-values-db
-codeql database create --language=cpp -s "$SRCDIR" -j 8 -v $DB --command="clang -fsyntax-only -Wno-unused-value $SRCDIR/DB/db.c"
+codeql database create --language=cpp -s "$SRCDIR" -j 8 -v $DB --command="clang -fsyntax-only -Wno-unused-value $SRCDIR/db.c"
